265a9f12b2e24c9ea774e5af2469dc51ef651a1c
vianapopatiya004-source <vianapopatiya004@gmail.com>
2025-12-26 13:00:20 +0530

    variables , keywords , print statements

--- intro.py
+++ intro.py
@@ -1,1 +1,21 @@
 print("Hello World!")
+print("Welcome to the world of Programming!")
+print(4)
+print("Hello Python \n")
+print("hello" , 5)
+print("Welcome to " , end="*")
+print("Test")
+
+x = 5 
+y = "John"
+print (x)
+print (y)
+
+c = "codingal"
+print (c)
+
+name = input("enter your name: ")
+print("\nHello", name, "\nwelcome to codingal")
+import keyword
+print("Python Keywords are: \n")
+print(keyword.kwlist)
