24b66af3175d4375d5446cb477a83bf720f3625e
vianapopatiya004-source <vianapopatiya004@gmail.com>
2025-12-27 17:16:47 +0530

    Data Types

---
+++ datatypes.py
@@ -0,0 +1,37 @@
+# a = 5 
+# print("type of a:", type(a))
+
+# b = 2.5 
+# print("type of b:", type(b))
+
+# c = "coding"
+# print("type of c:", type(c))
+
+# d = True 
+# print("type of d:", type(d)) 
+
+
+name = "Penguin"
+age = 15
+is_student = True
+weight = 38.5 
+
+print("name:", name)
+print("Data Type of name is", type(name))
+
+print("age:", age)
+print("Data Type of age is", type(age))
+
+print("is student:", is_student)
+print("Data Type of is_student is", type(is_student))
+
+print("weight:", weight)
+print("Data Type of weight is", type(weight))
+
+print("\n After Type Casting . . . ")
+age= str((age))
+print(age)
+print("Data Type of age is", type(age))
+weight = int(weight)
+print(weight)
+print("Data Type of weight is", type(weight))
