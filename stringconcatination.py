67872585a73fbe8ebfd2896f0c640172f07927e9
vianapopatiya004-source <vianapopatiya004@gmail.com>
2025-12-30 14:45:02 +0530

    String Concatination Project

---
+++ stringconcatination.py
@@ -0,0 +1,5 @@
+print("Welcome ! Generate cogratulation messages here!")
+cong_1 = input("Kindly enter the name of the person:")
+cong_1a = input("Kindly enter the reason:")
+
+print("Congratulations to ", cong_1, "for", cong_1a, "!")
